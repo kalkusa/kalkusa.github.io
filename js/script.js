94f48d8b48852a5bcc5971ae1b15bbcc38a9af3e
kalkusa <kalkus@10g.pl>
2018-01-13 21:30:26 +0100

    Added vanilla javascript navigation stub.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
-window.onload = function(){
-    //console.log('test');
+window.onload = function () {
+    this.currentSection = "main";
 };
+
+function navigate(sectionName) {
+    var section = document.querySelector('section#' +  this.currentSection);
+    section.classList.add('invisible');
+    section = document.querySelector('section#' + sectionName);
+    section.classList.remove('invisible');
+    this.currentSection = sectionName;
+}
