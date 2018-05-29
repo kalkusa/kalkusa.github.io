ea53bcc4eb0e80c783f2f178183c393435843703
kalkusa <kalkus@10g.pl>
2018-05-29 22:08:50 +0200

    Improve CV page. Rework tooltip algorithm. Add pinned-tooltip component.

--- js/script.js
+++ js/script.js
@@ -1,142 +1,155 @@
-'use strict';
+"use strict";
 
 var imagesCount;
 var loadedImagesCount = 0;
-var currentSection = "main"
+var currentSection = "main";
 var app;
 
+//Tooltips on CV
+Vue.component("pinned-tooltip", {
+  props: ["position", "company", "from", "to", "x", "y", "top", "left"],
+  template: `
+<div class="tip l-tip" :x="x" :y="y" :style="{ top: top + 'px', left:left+'px'}">
+<p>
+  <b>{{position}}</b>
+</p>
+<p>{{company}}</p>
+<p>{{from}} - {{to}}</p>
+</div>`,
+  created() {
+    window.addEventListener("resize", this.updateComponentPosition);
+    window.dispatchEvent(new Event('resize'));
+  },
+  destroyed() {
+    window.removeEventListener("resize", this.updateComponentPosition);
+    this.data();
+  },
+  methods: {
+    updateComponentPosition(e) {
+      var windowWidth = e.target.innerWidth;
+      var windowHeight = e.target.innerHeight;
+      var xScale = windowWidth / this.getBackgroundImageSize().width;
+      var yScale = windowHeight / this.getBackgroundImageSize().height;
+      var scale;
+      var yOffset = 0;
+      var xOffset = 0;
+
+      if (xScale > yScale) {
+        // The image fits perfectly in x axis, stretched in y
+        scale = xScale;
+        yOffset =
+          (windowHeight - this.getBackgroundImageSize().height * scale) / 2;
+      } else {
+        // The image fits perfectly in y axis, stretched in x
+        scale = yScale;
+        xOffset =
+          (windowWidth - this.getBackgroundImageSize().width * scale) / 2;
+      }
+
+      this.top = this.y * scale + yOffset;
+      this.left = this.x * scale + xOffset;
+    },
+    getBackgroundImageSize() {
+      //TODO "unmock" this later
+      return {
+        width: 4465,
+        height: 2827
+      };
+    }
+  }
+});
+
 window.onload = function () {
-    preloadImages(function onCompleted() {
-        onPageLoaded(500);
-        app = new Vue({
-            el: '#container',
-            data: {
-                experience: (new Date()).getFullYear() - 2013
-            }
-        });
+  preloadImages(function onCompleted() {
+    onPageLoaded(500);
+    app = new Vue({
+      el: "#container",
+      data: {
+        experience: new Date().getFullYear() - 2013
+      }
     });
+
+
+  });
 };
 
 function navigate(sectionName) {
-    var section = document.querySelector('section#' + currentSection);
-    section.classList.add('invisible');
-    section = document.querySelector('section#' + sectionName);
-    section.classList.remove('invisible');
-    currentSection = sectionName;
-    align();
+  var section = document.querySelector("section#" + currentSection);
+  section.classList.add("invisible");
+  section = document.querySelector("section#" + sectionName);
+  section.classList.remove("invisible");
+  currentSection = sectionName;
+  //align();
 }
 
 function onPageLoaded(timeout) {
-    setTimeout(() => {
-        let body = document.querySelector('body');
-        body.classList.add('loaded');
-    }, timeout);
+  setTimeout(() => {
+    let body = document.querySelector("body");
+    body.classList.add("loaded");
+  }, timeout);
 }
 
 function preloadImages(onCompleted) {
-    var images = Array.from(document.images).map(image => {
-        return image.src;
-    }).concat(getBackgroundImages());
-    console.log(images);
-    imagesCount = images.length;
-    loadedImagesCount = 0;
-    for (var imgNo in images) {
-        preloadImage(images[imgNo], function () {
-            loadedImagesCount++;
-            if (imagesCount == loadedImagesCount) {
-                onCompleted();
-            }
-        });
-    }
+  var images = Array.from(document.images)
+    .map(image => {
+      return image.src;
+    })
+    .concat(getBackgroundImages());
+  //console.log(images);
+  imagesCount = images.length;
+  loadedImagesCount = 0;
+  for (var imgNo in images) {
+    preloadImage(images[imgNo], function () {
+      loadedImagesCount++;
+      if (imagesCount == loadedImagesCount) {
+        onCompleted();
+      }
+    });
+  }
 }
 
 function preloadImage(url, callback) {
-    var img = new Image();
-    img.src = url;
-    img.onload = callback;
+  var img = new Image();
+  img.src = url;
+  img.onload = callback;
 }
 
 function getBackgroundImages() {
-    var url, B = [],
-        A = document.getElementsByTagName('*');
-    A = B.slice.call(A, 0, A.length);
-    while (A.length) {
-        url = document.deepCss(A.shift(), 'background-image');
-        if (url) url = /url\(['"]?([^")]+)/.exec(url) || [];
-        url = url[1];
-        if (url && B.indexOf(url) == -1) B[B.length] = url;
-    }
-    return B;
+  var url,
+    B = [],
+    A = document.getElementsByTagName("*");
+  A = B.slice.call(A, 0, A.length);
+  while (A.length) {
+    url = document.deepCss(A.shift(), "background-image");
+    if (url) url = /url\(['"]?([^")]+)/.exec(url) || [];
+    url = url[1];
+    if (url && B.indexOf(url) == -1) B[B.length] = url;
+  }
+  return B;
 }
 
 document.deepCss = function (who, css) {
-    if (!who || !who.style) return '';
-    var sty = css.replace(/\-([a-z])/g, function (a, b) {
-        return b.toUpperCase();
-    });
-    if (who.currentStyle) {
-        return who.style[sty] || who.currentStyle[sty] || '';
+  if (!who || !who.style) return "";
+  var sty = css.replace(/\-([a-z])/g, function (a, b) {
+    return b.toUpperCase();
+  });
+  if (who.currentStyle) {
+    return who.style[sty] || who.currentStyle[sty] || "";
+  }
+  var dv = document.defaultView || window;
+  return (
+    who.style[sty] || dv.getComputedStyle(who, "").getPropertyValue(css) || ""
+  );
+};
+
+Array.prototype.indexOf =
+  Array.prototype.indexOf ||
+  function (what, index) {
+    index = index || 0;
+    var L = this.length;
+    while (index < L) {
+      if (this[index] === what) return index;
+      ++index;
     }
-    var dv = document.defaultView || window;
-    return who.style[sty] ||
-        dv.getComputedStyle(who, "").getPropertyValue(css) || '';
-}
-
-Array.prototype.indexOf = Array.prototype.indexOf ||
-    function (what, index) {
-        index = index || 0;
-        var L = this.length;
-        while (index < L) {
-            if (this[index] === what) return index;
-            ++index;
-        }
-        return -1;
-    }
-
-//Tooltips on CV
-//TODO Rework to vanilla js
-
-//var initWidth = 1366;
-var initWidth = 1000;
-
-function setAnchorPointLeft(elem, y, x) {
-    var w = $(window).width();
-    var fraction = w / initWidth;
-    elem.css('top', y * fraction - elem.outerHeight() - 15.29 + 'px')
-    elem.css('left', x * fraction - elem.outerWidth() - 15.29 + "px")
-}
-
-function setAnchorPointRight(elem, y, x) {
-    var w = $(window).width();
-    var fraction = w / initWidth;
-
-    elem.css('top', y * fraction - elem.outerHeight() - 15.29 + 'px')
-    elem.css('right', x * fraction - elem.outerWidth() - 15.29 + "px")
-}
-
-function align() {
-    $('.r-tip').each(function (index, item) {
-        let y = $(item).attr('y');
-        let x = $(item).attr('x');
-        setAnchorPointRight($(item), y, x);
-    });
-
-    $('.l-tip').each(function (index, item) {
-        let y = $(item).attr('y');
-        let x = $(item).attr('x');
-        setAnchorPointLeft($(item), y, x);
-    });
-}
-
-$(window).resize(function () {
-    let container= $('#container');
-    let containerRatio = container.height()/container.width();
-    let cvBgImgRatio = 2827/4465;
-    if(containerRatio>=cvBgImgRatio){
-        //console.log('Image is still');
-    }else{
-        //console.log('Image is resizing');
-        align();
-    }
-    
-});
+    return -1;
+  };
